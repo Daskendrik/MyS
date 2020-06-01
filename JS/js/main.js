cf6646c9efd405b4afdbc5ba75819afcdd9ed5f3
Daskendrik <52924734+Daskendrik@users.noreply.github.com>
2020-06-01 22:45:23 +0300

    перенос функций

--- JS/js/main.js
+++ JS/js/main.js
@@ -194,3 +194,18 @@ function Les_11_1(a, b) {
         return b;
     }
 }
+
+function les_12() {
+    function ask(question, yes, no) {
+        if (confirm(question)) yes()
+            else no();
+        }
+        function showOk() {
+            alert( "Вы согласны." );
+        }
+        function showCancel() {
+            alert( "Вы отменили выполнение." );
+        }  
+    // использование: функции showOk, showCancel передаются в качестве аргументов ask
+            ask("Вы согласны?", showOk, showCancel);
+        }
