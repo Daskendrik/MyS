dde34b93730684ff62d03c9bc6ecec8023302b19
Dasha <52924734+Daskendrik@users.noreply.github.com>
2020-07-25 17:42:39 +0300

    поменяла структуру добавила кое че

--- JS/js/main.js
+++ JS/js/main.js
@@ -417,4 +417,28 @@ function les_17() {
     }
 
 }
+
+function les_18() {
+    let calculator = {
+        a: 0,
+        b: 0,
+        read() { 
+            this.a = +prompt('Введите значение а:', [0]);
+            this.b = +prompt('Введите значение b:', [0]);
+        },
+
+        sum() { 
+            return this.a + this.b;
+        },
+
+        mul() {
+            return  this.a * this.b;
+        },
+
+    };
+
+    calculator.read();
+    alert( calculator.sum() );
+    alert( calculator.mul() );
+}
     
