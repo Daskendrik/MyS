034b90187ee0bd20e48377794ad7b1c235fcd5d5
Dasha <52924734+Daskendrik@users.noreply.github.com>
2021-01-29 20:08:42 +0300

    add info

---
+++ JSprogramm/Program2/index.js
@@ -0,0 +1,12 @@
+let $gameBoarde = document.querySelector('#gameBoarde')
+let $start = document.querySelector('#start')
+
+$start.addEventListener('click',startGame)
+
+console.log($gameBoarde)
+
+function startGame() {
+    let a = 8; // кубиков
+    //рассталвяем кубики по сетке
+    
+}
